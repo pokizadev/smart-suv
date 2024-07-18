db648e58354006f4f9d2ef1fc8ae8b4b17e3065c
PokizaSdv <pokizasdv@gmail.com>
2024-07-17 22:37:55 -0400

    Create Container and Content components

--- src/landing-page/top-navigation/TopNavigation.tsx
+++ src/landing-page/top-navigation/TopNavigation.tsx
@@ -1,8 +1,16 @@
+import styled from "styled-components"
+import { Container } from "../components/Container"
+
+const LogoWrapper = styled.div`
+    width: 20%;
+`
 const TopNavigation = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <LogoWrapper>
+
+            </LogoWrapper>
+        </Container>
     )
 }
 
