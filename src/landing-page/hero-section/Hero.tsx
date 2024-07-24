625d5a4d3ee25ee7ccc3e0846601d846cf8fc095
PokizaSdv <pokizasdv@gmail.com>
2024-07-23 23:54:47 -0400

    Add typewriter-effect to hero section text

--- src/landing-page/hero-section/Hero.tsx
+++ src/landing-page/hero-section/Hero.tsx
@@ -1,41 +1,58 @@
 import styled from "styled-components";
+import TypewriterComponent from "typewriter-effect";
 import { Container, Content } from "../components/Container";
-import background from "../../assets/hero/background-3.jpg";
+import background from "../../assets/hero/background-5.jpg";
 
 const HeroContainer = styled(Container)`
     background-image: url(${background});
     width: 100%;
-    height: 350rem;
+    height: 80vh;
     background-repeat: no-repeat;
-    background-size: contain;
-    background-position: top;
-`;
-
-const HeroLeft = styled.div`
-    width: 50%;
+    background-size: cover;
+    background-position: center;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
 `;
 
-const HeroText = styled.p`
+const HeroContent = styled(Content)`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 100%;
+    margin: 0;
+`;
+const HeroLeft = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    width: 55%;
+`;
+
+const HeroText = styled.div`
     font-weight: var(--font-weight-700);
-    font-size: var(--font-size-80);
-    line-height: var(--line-height-96);
+    font-size: 9rem;
     color: var(--white);
-    
-    padding-top: var(--space-80);
-    padding-bottom: var(--space-80);
+    display: flex;
+    text-align: left;
 `;
 
 const Hero = () => {
     return (
         <HeroContainer>
-            <Content>
+            <HeroContent>
                 <HeroLeft>
-                    <HeroText>Drink smart, Live smart!</HeroText>
+                    <HeroText>
+                        <TypewriterComponent
+                            options={{
+                                strings: ["Smart Decision", "Smart Water"],
+                                autoStart: true,
+                                loop: true
+                            }}
+                        />
+                    </HeroText>
                 </HeroLeft>
-            </Content>
+            </HeroContent>
         </HeroContainer>
     );
 };
