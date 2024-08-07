363380758234c44c64d326e1d844d071fcd81f81
PokizaSdv <pokizasdv@gmail.com>
2024-08-06 22:21:32 -0400

    Change hovered link styles and remove order button

--- src/landing-page/top-navigation/TopNavigation.tsx
+++ src/landing-page/top-navigation/TopNavigation.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState } from "react";
+
 import { Container, Content } from "../components/Container";
 import logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
@@ -7,7 +9,7 @@ import english from "../../assets/icons/united-kingdom.png";
 import uzbek from "../../assets/icons/uzbekistan.png";
 import russian from "../../assets/icons/russia.png";
 import languageIcon from "../../assets/icons/internet.png";
-import { useState } from "react";
+import chevronDown from "../../assets/icons/chevron-down-svgrepo-com.svg";
 
 export const links = [
     { text: "Home", link: "/" },
@@ -40,16 +42,29 @@ const NavigationLinks = styled.ul`
 `;
 
 const Link = styled(NavLink)`
-    font-size: 1.6rem;
+    font-size: 1.8rem;
     color: var(--text-color);
+    font-weight: var(--font-weight-500);
+    border-radius: 0.6rem;
+    background-position: center;
+    background-color: transparent;
+    padding-left: var(--space-8);
+    padding-right: var(--space-8);
+    padding-top: var(--space-4);
+    padding-bottom: var(--space-4);
+    /* padding: 0.8rem; */
 
     &:hover,
     &:focus {
-        color: var(--primary);
+        height: var(--space-36);
+        background-color: #f7f7f7;
     }
 
     &.active {
-        color: var(--primary);
+        height: var(--space-36);
+
+        border-radius: 0.6rem;
+        background-color: #f7f7f7;
     }
 `;
 
@@ -58,16 +73,37 @@ const ButtonsWrapper = styled.div`
     gap: var(--space-16);
 `;
 
-const LanguageButton = styled(Button)`
+const LanguageButton = styled.div`
     width: 7rem;
     position: relative;
+    display: flex;
+    /* justify-content: space-between; */
+    text-align: center;
+    align-items: center;
+    font-size: 1.8rem;
+    color: var(--text-color);
+    font-weight: var(--font-weight-500);
+    background-color: #f7f7f7;
+    border-radius: 0.6rem;
+    padding-left: var(--space-12);
+    padding-right: var(--space-8);
+    padding-top: var(--space-4);
+    padding-bottom: var(--space-4);
+
 `;
 
-const LanguageIcon = styled.img`
-    height: 1.7rem;
-    width: 1.7rem;
-    margin-right: 0.6rem;
-    vertical-align: top;
+// const LanguageIcon = styled.img`
+//     height: 1.7rem;
+//     width: 1.7rem;
+//     margin-right: 0.6rem;
+//     vertical-align: top;
+// `;
+
+const ChevronDown = styled.img`
+    height: 1.8rem;
+    width: 1.8rem;
+    margin-left: 0.8rem;
+
 `;
 
 const ModalWrapper = styled.div`
@@ -75,17 +111,19 @@ const ModalWrapper = styled.div`
     position: absolute;
     top: 94%;
     right: 0;
-    background-color: #ecf0f1;
+    background-color: #f7f7f7;
     border-radius: 0.6rem;
     border-top-right-radius: 0;
     border-top-left-radius: 0;
-
-    padding-top: var(--space-12);
-    padding-bottom: var(--space-12);
+    padding-left: var(--space-4);
+    padding-top: var(--space-8);
+    padding-bottom: var(--space-8);
     display: flex;
     flex-direction: column;
+    text-align: left;
+    align-items: flex-start;
     z-index: 1000;
-    gap: 1rem;
+    gap: 0.5rem;
 `;
 
 const TopNavigation = () => {
@@ -112,33 +150,29 @@ const TopNavigation = () => {
                         );
                     })}
                 </NavigationLinks>
-                <ButtonsWrapper>
                     <LanguageButton
-                        size="sm"
-                        color="gray"
                         onClick={handleLanguageIconOnClick}
                     >
-                        <LanguageIcon src={languageIcon} />
-                        EN
+                        {/* <LanguageIcon src={english} /> */}
+                        En
+                        <ChevronDown src={chevronDown} />
                         {showLanguages && (
                             <ModalWrapper>
                                 <Button size="sm" color="gray">
-                                    <LanguageIcon src={russian} />
-                                    RU
+                                    {/* <LanguageIcon src={russian} /> */}
+                                    Ru
                                 </Button>
 
                                 <Button size="sm" color="gray">
-                                    <LanguageIcon src={uzbek} />
-                                    UZ
+                                    {/* <LanguageIcon src={uzbek} /> */}
+                                    Uz
                                 </Button>
-
                             </ModalWrapper>
                         )}
                     </LanguageButton>
-                    <Button size="sm" color="primary">
+                    {/* <Button size="sm" color="primary">
                         Order Now
-                    </Button>
-                </ButtonsWrapper>
+                    </Button> */}
             </NavigationContent>
         </Container>
     );
